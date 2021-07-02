238dc5f12290aaf19d0b02e72c5306a6047c772b
HsOjo <1134031392@qq.com>
2021-07-02 11:06:54 +0800

    Add api code/msg record for response.

--- saika/controller/api.py
+++ saika/controller/api.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from saika.enums import API_NOT_FOUND
 from saika.exception import AppException, APIException
 from .web import WebController
+from .. import hard_code
 
 
 class APIController(WebController):
@@ -27,11 +28,18 @@ class APIController(WebController):
             traceback.print_exc(file=sys.stderr)
             return APIException(data=dict(exc=str(e)))
 
+    def _record_response(self, code, msg):
+        self.context.g_set(hard_code.GK_RESPONSE_CODE, code)
+        self.context.g_set(hard_code.GK_RESPONSE_MSG, msg)
+
     def response(self, code=0, msg=None, **data):
+        self._record_response(code, msg)
         return jsonify(code=code, msg=msg, data=data)
 
     def success(self, code=0, msg=None, **data):
+        self._record_response(code, msg)
         raise APIException(code, msg, data)
 
     def error(self, code=1, msg=None, **data):
+        self._record_response(code, msg)
         raise APIException(code, msg, data)
