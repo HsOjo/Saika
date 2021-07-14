03b864162062b54c063120846a88da90b06d52f9
HsOjo <1134031392@qq.com>
2021-07-14 16:41:23 +0800

    Support set order in service.

--- saika/service/__init__.py
+++ saika/service/__init__.py
@@ -5,14 +5,25 @@ from .forms import FieldOperateForm
 class Service:
     def __init__(self, model_class):
         self.model_class = model_class
+        self.order = None
+
+    def set_order(self, *order):
+        self.order = order
 
     @property
     def query(self):
         return db.query(self.model_class)
 
+    @property
+    def query_order(self):
+        query = self.query
+        if self.order:
+            query = query.order_by(*self.order)
+        return query
+
     def list(self, page, per_page, query=None, **kwargs):
         if query is None:
-            query = self.query
+            query = self.query_order
         return query.paginate(page, per_page)
 
     def item(self, id, query=None, **kwargs):
