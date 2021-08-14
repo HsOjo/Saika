0ed09f8f598ff9ad6e77e14c6a6d3aa4747d212a
HsOjo <1134031392@qq.com>
2021-08-14 21:42:07 +0800

    Refactor curd service.

--- saika/service/__init__.py
+++ saika/service/__init__.py
@@ -39,34 +39,51 @@ class Service:
         field = getattr(self.model_class, pk)
         return field
 
-    def list(self, page, per_page, query=None, **kwargs):
-        if query is None:
-            query = self.query_order
-        return query.paginate(page, per_page)
+    def pk_filter(self, *ids):
+        if len(ids) == 1:
+            return self.pk_field.__eq__(*ids)
+        else:
+            return self.pk_field.in_(ids)
 
-    def item(self, id, query=None, **kwargs):
+    def _process_query(self, query=None, *processes):
         if query is None:
-            query = self.query_filter
-        return query.filter(self.pk_field.__eq__(id)).first()
+            query = self.query
+
+        for process in processes:
+            if callable(process):
+                query = process(query)
+            else:
+                query = process
+
+        return query
+
+    def list(self, page, per_page, query_processes=(), **kwargs):
+        return self._process_query(
+            self.query_order, *query_processes
+        ).paginate(page, per_page)
+
+    def item(self, id, query_processes=(), **kwargs):
+        return self._process_query(
+            self.query_filter, *query_processes,
+            lambda query: query.filter(self.pk_filter(id))
+        ).first()
 
     def add(self, **kwargs):
         model = self.model_class(**kwargs)
         db.add_instance(model)
         return model
 
-    def edit(self, id, **kwargs):
-        result = self.query_filter.filter(self.pk_field.__eq__(id)).update(kwargs)
-        db.session.commit()
-        return result
+    def edit(self, *ids, query_processes=(), **kwargs):
+        return self._process_query(
+            self.query_filter, *query_processes,
+            lambda query: query.filter(self.pk_filter(*ids))
+        ).update(kwargs)
 
-    def delete(self, id, **kwargs):
-        return self.delete_multiple([id], **kwargs)
-
-    def delete_multiple(self, ids, query=None, **kwargs):
+    def delete(self, *ids, query_processes=(), **kwargs):
         if not ids:
-            return
-        if query is None:
-            query = self.query_filter
-        result = query.filter(self.pk_field.in_(ids)).delete()
-        db.session.commit()
-        return result
+            return 0
+
+        return self._process_query(
+            self.query_filter, *query_processes,
+            lambda query: query.filter(self.pk_filter(*ids))
+        ).delete()
