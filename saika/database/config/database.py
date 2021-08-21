29bc8a73d20079b97762bb4965d98ebd7ec14c8d
HsOjo <1134031392@qq.com>
2021-08-22 03:53:00 +0800

    Remove invalid config.

--- saika/database/config/database.py
+++ saika/database/config/database.py
@@ -3,19 +3,14 @@ from saika import BaseConfig
 
 class DatabaseConfig(BaseConfig):
     driver = 'sqlite'
+
     query_args = {}
 
     echo_sql = False
-
-    pool_size = None
-    pool_timeout = None
-
     track_modifications = False
 
     def merge(self) -> dict:
         return dict(
             SQLALCHEMY_ECHO=self.echo_sql,
-            SQLALCHEMY_POOL_SIZE=self.pool_size,
-            SQLALCHEMY_POOL_TIMEOUT=self.pool_timeout,
             SQLALCHEMY_TRACK_MODIFICATIONS=self.track_modifications,
         )
