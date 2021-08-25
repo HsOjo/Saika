6e84a37a277745f0e5754af36a7995ae2dd85ead
HsOjo <1134031392@qq.com>
2021-08-25 22:02:32 +0800

    Rename view context key.

--- saika/controller/view.py
+++ saika/controller/view.py
@@ -9,11 +9,11 @@ from .web import WebController
 
 class ViewControlller(WebController):
     def assign(self, **kwargs):
-        context = self.context.g_get(hard_code.GK_CONTEXT)
-        if context is None:
-            context = {}
-            self.context.g_set(hard_code.GK_CONTEXT, context)
-        context.update(kwargs)
+        view_context = self.context.g_get(hard_code.GK_VIEW_CONTEXT)
+        if view_context is None:
+            view_context = {}
+            self.context.g_set(hard_code.GK_VIEW_CONTEXT, view_context)
+        view_context.update(kwargs)
 
     def fetch(self, template=None):
         if template is None:
@@ -32,5 +32,5 @@ class ViewControlller(WebController):
             template = re.sub('/+', '/', template)
             template = '%s.html' % template.strip('/')
 
-        context = self.context.g_get(hard_code.GK_CONTEXT, {})
-        return render_template(template, **context)
+        view_context = self.context.g_get(hard_code.GK_VIEW_CONTEXT, {})
+        return render_template(template, **view_context)
